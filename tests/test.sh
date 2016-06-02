dbe110e8554d047a0b2029914958fb4dd3c6079b
Marius van den Beek <m.vandenbeek@gmail.com>
2016-06-02 19:46:35 +0200

    Fix basic test procedure.
    Adding third node fails for an unknown reason.

--- tests/test.sh
+++ tests/test.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
-docker network create --subnet=192.168.1.0/24 net1
-docker run -d --net=net1 --ip=192.168.1.10  --name etcd quay.io/coreos/etcd:v2.3.6
-docker run -d --name=pcs1 --security-opt seccomp:unconfined -e ETCD="192.168.1.10:2379" -e "MY_IP=192.168.1.20" --net=net1 --ip 192.168.1.20 --cap-add=NET_ADMIN  -v /sys/fs/cgroup:/sys/fs/cgroup:ro -e "BOOTSTRAP=True" pcs
-docker run -d --name=pcs2 --security-opt seccomp:unconfined -e ETCD="192.168.1.10:2379" -e "MY_IP=192.168.1.21" --net=net1 --ip 192.168.1.21 --cap-add=NET_ADMIN  -v /sys/fs/cgroup:/sys/fs/cgroup:ro pcs
-docker run -d --name=pcs3 --security-opt seccomp:unconfined -e ETCD="192.168.1.10:2379" -e "MY_IP=192.168.1.22" --net=net1 --ip 192.168.1.22 --cap-add=NET_ADMIN  -v /sys/fs/cgroup:/sys/fs/cgroup:ro pcs
-sleep 30s && docker exec -it pcs3 pcs status
+docker network create --subnet=192.168.10.0/24 net1
+export HostIP=192.168.10.10
+docker run -d --net net1 --ip 192.168.10.10 \
+ --name etcd quay.io/coreos/etcd \
+ -name etcd0 \
+ -advertise-client-urls http://${HostIP}:2379,http://${HostIP}:4001 \
+ -listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001 \
+ -initial-advertise-peer-urls http://${HostIP}:2380 \
+ -listen-peer-urls http://0.0.0.0:2380 \
+ -initial-cluster-token etcd-cluster-1 \
+ -initial-cluster etcd0=http://${HostIP}:2380 \
+ -initial-cluster-state new
+sleep 2s
+docker exec etcd /etcdctl set /hacluster/user hacluster
+docker exec etcd /etcdctl set /hacluster/password pass
+docker run -d --name=pcs1 --security-opt seccomp:unconfined -e ETCD="192.168.10.10:2379" -e "MY_IP=192.168.10.20" --net=net1 --ip 192.168.10.20 --cap-add=NET_ADMIN  -v /sys/fs/cgroup:/sys/fs/cgroup:ro -e "BOOTSTRAP=True" pcs
+sleep 90s
+docker run -d --name=pcs2 --security-opt seccomp:unconfined -e ETCD="192.168.10.10:2379" -e "MY_IP=192.168.10.21" --net=net1 --ip 192.168.10.21 --cap-add=NET_ADMIN  -v /sys/fs/cgroup:/sys/fs/cgroup:ro pcs
+#sleep 90s
+#docker run -d --name=pcs3 --security-opt seccomp:unconfined -e ETCD="192.168.10.10:2379" -e "MY_IP=192.168.10.22" --net=net1 --ip 192.168.10.22 --cap-add=NET_ADMIN  -v /sys/fs/cgroup:/sys/fs/cgroup:ro pcs
+sleep 90s && docker exec pcs2 journalctl && docker exec pcs2 pcs status
+
