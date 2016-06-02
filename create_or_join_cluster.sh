85dcde6fd0f373f5ff1ec180ce7fb499a37afd86
Marius van den Beek <m.vandenbeek@gmail.com>
2016-06-02 10:48:06 +0200

    Quote variable for string comparison

--- create_or_join_cluster.sh
+++ create_or_join_cluster.sh
@@ -4,11 +4,10 @@ source /etc/sysconfig/my_ip
 source /etc/sysconfig/etcd
 source /etc/sysconfig/bootstrap
 
-if [ $BOOTSTRAP == "True" ]
+if [ "$BOOTSTRAP" == "True" ]
   then
     pcs-etcd create --etcd_nodes "$ETCD" --my_ip "$MY_IP"
-    /usr/bin/crm configure load update /tmp/cib.txt
   else
-    /usr/bin/pcs-etcd join --etcd_nodes "$ETCD" --my_ip "$MY_IP"
+    pcs-etcd join --etcd_nodes "$ETCD" --my_ip "$MY_IP"
 fi
 systemd-notify READY=1
