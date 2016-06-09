3e92df76eba71035f7d901798921c5c38a6d0000
Marius van den Beek <m.vandenbeek@gmail.com>
2016-06-09 19:59:19 +0200

    Set node TTL to 1 if explicitly stopping container
    Nodes will be removed from the cluster if their TTL in nodes/<ip/hostname> runs
    out (default to 60 seconds).  If we're stopping the watch daemon (which also
    extends the TTL), we set the TTL to 1.  In testing, use a fixed docker hostname
    instead of an IP, so that rejoining works properly.

--- tests/test.sh
+++ tests/test.sh
@@ -17,16 +17,16 @@ docker exec etcd /etcdctl set /hacluster/user hacluster
 docker exec etcd /etcdctl set /hacluster/password pass
 docker exec etcd /etcdctl mkdir /hacluster/nodes
 echo "Testing start of 5 node cluster"
-docker run -d --name=pcs1 --security-opt seccomp:unconfined -e ETCD="192.168.10.10:2379" -e "MY_IP=192.168.10.20" --net=net1 --ip 192.168.10.20 --cap-add=NET_ADMIN  -v /sys/fs/cgroup:/sys/fs/cgroup:ro pcs
-docker run -d --name=pcs2 --security-opt seccomp:unconfined -e ETCD="192.168.10.10:2379" -e "MY_IP=192.168.10.21" --net=net1 --ip 192.168.10.21 --cap-add=NET_ADMIN  -v /sys/fs/cgroup:/sys/fs/cgroup:ro pcs
-docker run -d --name=pcs3 --security-opt seccomp:unconfined -e ETCD="192.168.10.10:2379" -e "MY_IP=192.168.10.22" --net=net1 --ip 192.168.10.22 --cap-add=NET_ADMIN  -v /sys/fs/cgroup:/sys/fs/cgroup:ro pcs
-docker run -d --name=pcs4 --security-opt seccomp:unconfined -e ETCD="192.168.10.10:2379" -e "MY_IP=192.168.10.23" --net=net1 --ip 192.168.10.23 --cap-add=NET_ADMIN  -v /sys/fs/cgroup:/sys/fs/cgroup:ro pcs
-docker run -d --name=pcs5 --security-opt seccomp:unconfined -e ETCD="192.168.10.10:2379" -e "MY_IP=192.168.10.24" --net=net1 --ip 192.168.10.24 --cap-add=NET_ADMIN  -v /sys/fs/cgroup:/sys/fs/cgroup:ro pcs
+docker run -d --stop-signal=RTMIN+3 --name=pcs1 -h pcs1 --security-opt seccomp:unconfined -e ETCD="192.168.10.10:2379" -e "MY_IP=pcs1" --net=net1 --ip 192.168.10.20 --cap-add=NET_ADMIN  -v /sys/fs/cgroup:/sys/fs/cgroup:ro pcs
+docker run -d --stop-signal=RTMIN+3 --name=pcs2 -h pcs2 --security-opt seccomp:unconfined -e ETCD="192.168.10.10:2379" -e "MY_IP=pcs2" --net=net1 --ip 192.168.10.21 --cap-add=NET_ADMIN  -v /sys/fs/cgroup:/sys/fs/cgroup:ro pcs
+docker run -d --stop-signal=RTMIN+3 --name=pcs3 -h pcs3 --security-opt seccomp:unconfined -e ETCD="192.168.10.10:2379" -e "MY_IP=pcs3" --net=net1 --ip 192.168.10.22 --cap-add=NET_ADMIN  -v /sys/fs/cgroup:/sys/fs/cgroup:ro pcs
+docker run -d --stop-signal=RTMIN+3 --name=pcs4 -h pcs4 --security-opt seccomp:unconfined -e ETCD="192.168.10.10:2379" -e "MY_IP=pcs4" --net=net1 --ip 192.168.10.23 --cap-add=NET_ADMIN  -v /sys/fs/cgroup:/sys/fs/cgroup:ro pcs
+docker run -d --stop-signal=RTMIN+3 --name=pcs5 -h pcs5 --security-opt seccomp:unconfined -e ETCD="192.168.10.10:2379" -e "MY_IP=pcs5" --net=net1 --ip 192.168.10.24 --cap-add=NET_ADMIN  -v /sys/fs/cgroup:/sys/fs/cgroup:ro pcs
 sleep 80s && docker exec pcs2 journalctl && docker exec pcs2 pcs status
 [ `docker exec -it pcs3 pcs status | grep Online |wc -l` == 6 ] && echo "Cluster started sucessfully"
 echo "Test removing node"
-docker rm -f pcs3
-sleep 65s && [ `docker exec -it pcs2 pcs status | grep Online |wc -l` == 5 ] && echo "Node removed successfully"
-docker run -d --name=pcs3 --security-opt seccomp:unconfined -e ETCD="192.168.10.10:2379" -e "MY_IP=192.168.10.22" --net=net1 --ip 192.168.10.22 --cap-add=NET_ADMIN  -v /sys/fs/cgroup:/sys/fs/cgroup:ro pcs
+docker stop pcs3 && docker rm pcs3
+sleep 5s && [ `docker exec -it pcs2 pcs status | grep Online |wc -l` == 5 ] && echo "Node removed successfully"
+docker run -d --stop-signal=RTMIN+3 --name=pcs3 -h pcs3 --security-opt seccomp:unconfined -e ETCD="192.168.10.10:2379" -e "MY_IP=pcs3" --net=net1 --ip 192.168.10.22 --cap-add=NET_ADMIN  -v /sys/fs/cgroup:/sys/fs/cgroup:ro pcs
 sleep 30s && docker exec pcs2 journalctl && docker exec pcs2 pcs status
 [ `docker exec -it pcs3 pcs status | grep Online |wc -l` == 6 ] && echo "Node joined back sucesfully"
